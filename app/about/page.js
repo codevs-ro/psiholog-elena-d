b93760b344f3653c07e5fe2c878ac57cd8573d14
Grosu Antonio <codevs@Grosus-MacBook-Air.local>
2024-09-02 12:23:56 +0300

    first commit

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,9 @@
-const HomePage = () => {
+const AboutPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> Home Page</h1>
+      <h1> About Page</h1>
     </main>
   );
 };
 
-export default HomePage;
+export default AboutPage;
--- app/kit/page.js
+++ app/kit/page.js
@@ -1,9 +1,9 @@
-const HomePage = () => {
+const KitPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> Home Page</h1>
+      <h1> Kit Page</h1>
     </main>
   );
 };
 
-export default HomePage;
+export default KitPage;
